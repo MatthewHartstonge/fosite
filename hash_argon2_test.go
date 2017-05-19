592abebf1fa6aad9e68138391ec29db4820f5b66
Matthew Hartstonge <matt@linc-ed.com>
2017-05-19 17:33:06 +1200

    Increase Argon2 test quality

--- hash_argon2_test.go
+++ hash_argon2_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// TestArgon2Hash ensures that a hash is returned from the Argon2 Hasher
 func TestArgon2Hash(t *testing.T) {
 	h := &Argon2{
 		Config: argon2.DefaultConfig(),
@@ -19,6 +20,20 @@ func TestArgon2Hash(t *testing.T) {
 	assert.NotEqual(t, hash, password)
 }
 
+// TestArgon2HashLibErr purposely causes the underlying argon2 lib to error to ensure it is reported up the stack
+func TestArgon2HashLibErr(t *testing.T) {
+	h := &Argon2{
+		Config: argon2.DefaultConfig(),
+	}
+	h.Config.MemoryCost = 1
+	password := []byte("foo")
+	hash, err := h.Hash(password)
+	assert.Empty(t, hash)
+	assert.NotNil(t, err)
+	assert.NotEqual(t, hash, password)
+}
+
+// TestArgon2CompareEquals ensures a password can be verified successfully when decoded
 func TestArgon2CompareEquals(t *testing.T) {
 	h := &Argon2{
 		Config: argon2.DefaultConfig(),
@@ -31,6 +46,7 @@ func TestArgon2CompareEquals(t *testing.T) {
 	assert.Nil(t, err)
 }
 
+// TestArgon2CompareEquals ensures a compare errors when a presented clear text password does not match the original
 func TestArgon2CompareDifferent(t *testing.T) {
 	h := &Argon2{
 		Config: argon2.DefaultConfig(),
@@ -39,6 +55,20 @@ func TestArgon2CompareDifferent(t *testing.T) {
 	hash, err := h.Hash(password)
 	assert.Nil(t, err)
 	assert.NotNil(t, hash)
+	err = h.Compare(hash, []byte(uuid.NewRandom()))
+	assert.NotNil(t, err)
+}
+
+// TestArgon2HashLibErr purposely causes the underlying argon2 lib to error to ensure it is reported up the stack
+func TestArgon2CompareLibErr(t *testing.T) {
+	h := &Argon2{
+		Config: argon2.DefaultConfig(),
+	}
+	h.Config.MemoryCost = 1
+	password := []byte("foo")
+	hash, err := h.Hash(password)
+	assert.Empty(t, hash)
+	assert.NotNil(t, err)
 	err = h.Compare(hash, []byte(uuid.NewRandom()))
 	assert.NotNil(t, err)
 }
